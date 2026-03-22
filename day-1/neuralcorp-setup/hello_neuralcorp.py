ac3b05ca9be8666f2b8a92e1bae48f66cb25fe02
Edward <edpraveen1@gmail.com>
2026-03-23 00:23:02 +0530

    updated with modified contents

---
+++ day-1/neuralcorp-setup/hello_neuralcorp.py
@@ -0,0 +1,8 @@
+import sys
+import platform
+
+
+print("Hello, NeuralCorp!")
+print(f"Python version: {sys.version}")
+print(f"Platform: {platform.machine()}")
+print(f"Mac version: {platform.mac_ver()}")
