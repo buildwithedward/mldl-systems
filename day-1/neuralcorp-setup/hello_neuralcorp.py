994f546901d289547e91433bdeb7ab8b15ae3440
Edward <edpraveen1@gmail.com>
2026-04-26 23:01:01 +0530

    starting from scratch

--- day-1/neuralcorp-setup/hello_neuralcorp.py
+++
@@ -1,8 +0,0 @@
-import sys
-import platform
-
-
-print("Hello, NeuralCorp!")
-print(f"Python version: {sys.version}")
-print(f"Platform: {platform.machine()}")
-print(f"Mac version: {platform.mac_ver()}")
--- day-11/src/neuralcorp/fetcher/__init__.py
+++
@@ -1,4 +0,0 @@
-from neuralcorp.fetcher.client import fetch_many, fetch_one
-from neuralcorp.fetcher.models import FetchResult
-
-__all__ = ["fetch_many", "fetch_one", "FetchResult"]
